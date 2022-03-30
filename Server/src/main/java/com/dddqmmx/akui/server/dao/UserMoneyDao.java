d6ae91fe44b9c23189d61cf45ac8097cfb657370
dddqmmx <1538080366@qq.com>
2022-03-30 19:25:43 +0800

    商店和金钱更新

--- Server/src/main/java/com/dddqmmx/akui/server/dao/UserMoneyDao.java
+++ Server/src/main/java/com/dddqmmx/akui/server/dao/UserMoneyDao.java
@@ -1,6 +1,10 @@
 package com.dddqmmx.akui.server.dao;
 
+import java.util.List;
+import java.util.Map;
+
 public interface UserMoneyDao {
-    String getMoney(long qq,int itemId);
+    int getMoney(long qq,int itemId);
     int setMoney();
+    Map<String, String> moneyList(long qq);
 }
--- Server/src/main/java/com/dddqmmx/akui/server/dao/impl/UserMoneyImpl.java
+++ Server/src/main/java/com/dddqmmx/akui/server/dao/impl/UserMoneyImpl.java
@@ -3,18 +3,47 @@ package com.dddqmmx.akui.server.dao.impl;
 import com.dddqmmx.akui.server.dao.BaseDao;
 import com.dddqmmx.akui.server.dao.UserMoneyDao;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
 public class UserMoneyImpl extends BaseDao implements UserMoneyDao {
 
     @Override
-    public String getMoney(long qq,int itemId) {
+    public int getMoney(long qq,int itemId) {
         String sql = "select number from user_money where qq = ? and moneyId = ?";
-        Object[] obj = {qq,itemId};
-
-        return null;
+        Object[] objects = {qq,itemId};
+        ResultSet resultSet = executeQuery(sql,objects);
+        try {
+            while (resultSet.next()){
+                return resultSet.getInt("number");
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return 0;
     }
 
     @Override
     public int setMoney() {
         return 0;
     }
+
+    @Override
+    public Map<String, String> moneyList(long qq) {
+        String sql = "select moneyId,number from user_money where qq = ?";
+        Object[] objects = {qq};
+        ResultSet resultSet = executeQuery(sql,objects);
+        Map<String, String> map = null;
+        try {
+            while (resultSet.next()){
+                map.put(resultSet.getString("moneyId"),resultSet.getString("number"));
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return map;
+    }
+
 }
